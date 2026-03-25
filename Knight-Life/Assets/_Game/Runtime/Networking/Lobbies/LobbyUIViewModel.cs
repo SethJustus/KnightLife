9e43ab37ed7d4b4a042fd3e2e60ae1c573bce6c0
Seth Barrere <105650714+SethJustus@users.noreply.github.com>
2026-03-24 21:16:55 -0400

    Lobbies List

--- Knight-Life/Assets/_Game/Runtime/Networking/Lobbies/LobbyUIViewModel.cs
+++ Knight-Life/Assets/_Game/Runtime/Networking/Lobbies/LobbyUIViewModel.cs
@@ -1,14 +1,19 @@
 namespace KnightLife.Runtime.Networking.Lobbies
 {
-    using UnityEngine;
-    using UnityEngine.UIElements;
-    using Unity.Services.Lobbies;
-    using Unity.Services.Lobbies.Models;
+    using NUnit.Framework;
+    using System.Collections;
     using System.Collections.Generic;
     using System.Threading.Tasks;
+    using Unity.Properties;
+    using Unity.Services.Lobbies;
+    using Unity.Services.Lobbies.Models;
+    using UnityEngine;
+    using UnityEngine.UIElements;
+    using static UnityEditor.Profiling.HierarchyFrameDataView;
 
     public class LobbyUIViewModel : MonoBehaviour
     {
+        [UxmlAttribute, CreateProperty]
         public List<Lobby> Lobbies { get; private set; }
 
         private MultiColumnListView lobbyList;
@@ -19,16 +24,45 @@ namespace KnightLife.Runtime.Networking.Lobbies
             var root = GetComponent<UIDocument>().rootVisualElement;
 
             // Find the button by name
-            Button button = root.Q<Button>("my-button");
-            Button refreshButton = root.Q<Button>("refresh-lobbies");
-            this.lobbyList = root.Q<MultiColumnListView>("lobby-list");
-
+            Button button = root.Q<Button>("create-lobby-button");
+            Button refreshButton = root.Q<Button>("refresh-lobbies-button");
+            
             // Add click behavior
             button.clicked += OnCreateLobbyButton_Clicked;
             refreshButton.clicked += GetLobbies;
 
-            Debug.Log("About to run get lobbies");
-            //Task.Run(this.GetLobbies);
+            this.lobbyList = root.Q<MultiColumnListView>("lobby-list");
+
+            // 1) Set the object that owns the list
+            lobbyList.dataSource = this;
+
+            // 2) Bind itemsSource to the list property on that object
+            lobbyList.SetBinding("itemsSource", new DataBinding
+            {
+                dataSourcePath = new PropertyPath(nameof(this.Lobbies))
+            });
+
+            // 3) Tell each column how to display a row item
+            lobbyList.columns["name"].makeCell = () => new Label();
+            lobbyList.columns["name"].bindCell = (element, rowIndex) =>
+            {
+                var label = (Label)element;
+                label.text = Lobbies[rowIndex].Name;
+            };
+
+            lobbyList.columns["max-players"].makeCell = () => new Label();
+            lobbyList.columns["max-players"].bindCell = (element, rowIndex) =>
+            {
+                var label = (Label)element;
+                label.text = Lobbies[rowIndex].MaxPlayers.ToString();
+            };
+
+            lobbyList.columns["current-players"].makeCell = () => new Label();
+            lobbyList.columns["current-players"].bindCell = (element, rowIndex) =>
+            {
+                var label = (Label)element;
+                label.text = Lobbies[rowIndex].Players.Count.ToString();
+            };
         }
 
         public async void GetLobbies()
